7267dd270af36ecb1617f961eeff6218c757ad07
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-29 18:41:57 -0700

    directories set up and reducer file base setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { reducer } from './store/reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, )
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
